2fc58f361dc5120d9921424de958aca02b6ab0be
Aleksandr Chatsky <aleksandr.chatsky@glia.com>
2025-01-31 16:41:15 +0200

    First commit

--- app/src/main/java/com/gugalo/puzzle_game_tutorial/LevelOneActivity.kt
+++ app/src/main/java/com/gugalo/puzzle_game_tutorial/LevelOneActivity.kt
@@ -1,7 +1,9 @@
 package com.gugalo.puzzle_game_tutorial
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LevelOneActivity : AppCompatActivity() {
@@ -10,9 +12,30 @@ class LevelOneActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level_one)
 
+        val clickCountTextView = findViewById<TextView>(R.id.clickCounterTextView)
+        val clickMeButton = findViewById<Button>(R.id.clickMeButton)
         val nextLevelButton = findViewById<Button>(R.id.nextButton)
 
         var clickCount = 0
         val requiredClicksCount = 100
+
+        // Update the text showing the click count
+        fun updateClickCounterLabel() {
+            clickCountTextView.text = "$clickCount / $requiredClicksCount"
+        }
+
+        // Initialize the text
+        updateClickCounterLabel()
+
+        // Handle click on the big button
+        clickMeButton.setOnClickListener {
+            clickCount++
+            updateClickCounterLabel()
+
+            // If we’ve reached 100 clicks, enable the "Next" button
+            if (clickCount >= requiredClicksCount) {
+                nextLevelButton.isEnabled = true
+            }
+        }
     }
 }
